55edb31e19a83db9615065944fbe392dccaf2182
shamimhost <shamimhosting@outlook.com>
2024-08-21 12:26:31 +0330

    1st

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/shamimhost/gomod
+module mymodul
 
 go 1.21.5
+
+require github.com/shamimhost/puppy v0.0.0-20240821074602-e5929f8b09a0 // indirect
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/shamimhost/puppy"
+)
+
+func main() {
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+
+	println(s1)
+	println(s2)
+}
